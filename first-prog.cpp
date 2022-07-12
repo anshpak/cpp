9a6752ff29362b77e309f1aa5640d905977ae547
Andrey Shpak <anshpak.by@gmail.com>
2022-07-12 21:50:06 +0300

    refactor: add spaces between >> and << operators

--- first-prog.cpp
+++ first-prog.cpp
@@ -3,20 +3,20 @@
 
 int main() {
     int a, b, r;
-    std::cout<<"The first number: ";
-    std::cin>>a;
-    std::cout<<"The second number: ";
-    std::cin>>b;
-    std::cout<<"Result of ";
+    std::cout << "The first number: ";
+    std::cin >> a;
+    std::cout << "The second number: ";
+    std::cin >> b;
+    std::cout << "Result of ";
     if(a > 0 && b > 0) {
         r = a * b;
         std::cout << "multiplication: ";
     }
     else {
         r = a + b;
-        std::cout<<"sum: ";
+        std::cout << "sum: ";
     }
-    std::cout<<r;
+    std::cout << r;
     getch();
     return 0;
 }
