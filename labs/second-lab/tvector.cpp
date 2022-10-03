485d34db7fd97fee8b580fdedd6d2f872112df07
Andrey Shpak <anshpak.by@gmail.com>
2022-10-04 01:51:38 +0300

    refactor: delete unnecessary comments and add the new ones

--- labs/second-lab/tvector.cpp
+++ labs/second-lab/tvector.cpp
@@ -43,14 +43,6 @@ string Tvector::get_name() {
 	return name;
 }
 
-void Tvector::show_vector() {
-	cout << name << " = { ";
-	for (int i = 0; i < vector_size; i++) {
-		cout << values[i] << " ";
-	}
-	cout << "}\n";
-}
-
 int Tvector::get_X_amount() {
 	int count = 0;
 	for (int i = 0; i < vector_size; i++) {
@@ -61,6 +53,16 @@ int Tvector::get_X_amount() {
 	return count;
 }
 
+// this method has an analogue in the form of overloaded << operation
+void Tvector::show_vector() {
+	cout << name << " = { ";
+	for (int i = 0; i < vector_size; i++) {
+		cout << values[i] << " ";
+	}
+	cout << "}\n";
+}
+
+// this method has an analogue in the form of overloaded * operation
 bool are_orthogonal(Tvector v1, Tvector v2) {
 	int v1_values[vector_size], v2_values[vector_size];
 	v1.get_values(v1_values);
@@ -73,6 +75,7 @@ bool are_orthogonal(Tvector v1, Tvector v2) {
 	return true;
 }
 
+// this method has an analogue in the form of overloaded == operation
 bool are_equal(Tvector v1, Tvector v2) {
 	int v1_values[vector_size], v2_values[vector_size];
 	v1.get_values(v1_values);
@@ -85,6 +88,7 @@ bool are_equal(Tvector v1, Tvector v2) {
 	return true;
 }
 
+// this method has an analogue with a new realization in the form of overloaded / operation
 void show_vectors_intersection(Tvector v1, Tvector v2) {
 	int v1_values[vector_size], v2_values[vector_size];
 	v1.get_values(v1_values);
@@ -157,102 +161,3 @@ void show_menu() {
 	cout << "5 - to show how many times " << X << " appears in random vector;\n";
 	cout << "q - to quit.\n";
 }
-
-//house::house() {
-//	address = "";
-//	fl_num = 0;
-//	fl.rooms_amount = 0;
-//	fl.area = 0;
-//}
-//
-//house::house(string my_address, int my_fl_num, int my_rooms_amount, int my_area) {
-//	set_address(my_address);
-//	set_fl_num(my_fl_num);
-//	set_rooms_amount(my_rooms_amount);
-//	set_area(my_area);
-//}
-//
-//void house::show() {
-//	cout << "________________________________________" << endl;
-//	cout << "the address is " << address << ";" << endl;
-//	cout << "the current floor is " << fl_num << ";" << endl;
-//	cout << "there are " << fl.rooms_amount << " rooms;" << endl;
-//	cout << "the floor area is about " << fl.area << " square meters;" << endl;
-//}
-//
-//void house::set_address(string my_address) {
-//	address = my_address;
-//}
-//
-//void house::set_fl_num(int my_fl_num) {
-//	fl_num = my_fl_num;
-//}
-//
-//void house::set_rooms_amount(int my_rooms_amount) {
-//	fl.rooms_amount = my_rooms_amount;
-//}
-//
-//void house::set_area(int my_area) {
-//	fl.area = my_area;
-//}
-//
-//string house::get_address() {
-//	return address;
-//}
-//
-//int house::get_fl_num() {
-//	return fl_num;
-//}
-//
-//int house::get_rooms_amount() {
-//	return fl.rooms_amount;
-//}
-//
-//int house::get_area() {
-//	return fl.area;
-//}
-//
-//void show_stages_with_n_rooms(house flats[], const int n, int my_rooms_amount) {
-//	int temp;
-//	bool flag = false;
-//	for (int i = 1; i < n; i++) {
-//		temp = flats[i].get_rooms_amount();
-//		if (temp == my_rooms_amount) {
-//			cout << flats[i].get_address() << endl;
-//			flag = true;
-//		}
-//	}
-//	if (!flag) {
-//		cout << "none" << endl;
-//	}
-//}
-//
-//void show_stages_with_n_rooms_from_inter(house flats[], const int a, const int b, int my_rooms_amount) {
-//	int temp;
-//	bool flag = false;
-//	for (int i = a - 1; i < b; i++) {
-//		temp = flats[i].get_rooms_amount();
-//		if (temp == my_rooms_amount) {
-//			cout << flats[i].get_address() << endl;
-//			flag = true;
-//		}
-//	}
-//	if (!flag) {
-//		cout << "none" << endl;
-//	}
-//}
-//
-//void show_stages_with_area_more_than(house flats[], const int n, int my_area) {
-//	int temp;
-//	bool flag = false;
-//	for (int i = 0; i < n; i++) {
-//		temp = flats[i].get_area();
-//		if (temp > my_area) {
-//			cout << flats[i].get_address() << endl;
-//			flag = true;
-//		}
-//	}
-//	if (!flag) {
-//		cout << "none" << endl;
-//	}
-//}
--- labs/second-lab/tvector.h
+++ labs/second-lab/tvector.h
@@ -36,32 +36,4 @@ bool are_orthogonal(Tvector v1, Tvector v2);
 bool are_equal(Tvector v1, Tvector v2);
 void show_vectors_intersection(Tvector v1, Tvector v2);
 void show_menu();
-
-//struct stage {
-//	int rooms_amount;
-//	int area;
-//};
-//
-//class House {
-//	string address;
-//	int fl_num;
-//	stage fl;
-//public:
-//	House();
-//	House(string my_address, int my_fl_num, int my_rooms_amount, int my_area);
-//	void show();
-//	void set_address(string my_address);
-//	void set_fl_num(int my_fl_num);
-//	void set_rooms_amount(int my_rooms_amount);
-//	void set_area(int my_area);
-//	string get_address();
-//	int get_fl_num();
-//	int get_rooms_amount();
-//	int get_area();
-//};
-//
-//void show_stages_with_N_rooms(House flats[], const int N, int my_rooms_amount);
-//void show_stages_with_N_rooms_from_inter(House flats[], const int a, const int b, int my_rooms_amount);
-//void show_stages_with_area_more_than(House flats[], const int N, int my_area);
-
 #endif
