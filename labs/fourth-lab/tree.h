53e5dd8e77d08c6ef8774bc1fc95b2a6ea8a562c
Andrey Shpak <anshpak.by@gmail.com>
2022-11-06 14:49:52 +0300

    feat: add templates to the header

--- labs/fourth-lab/tree.h
+++ labs/fourth-lab/tree.h
@@ -1,1 +1,43 @@
-#pragma once
+// объявление класса и функций
+// функции определяю в tvector.cpp
+#ifndef TREE_H // предотвращение многократного включения
+#define TREE_H // заголовочного файла
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+/* #ifndef и #endif - директивы препроцессора, которые предотвращают многократное включение кода, расположенного между ними.
+Если загловок еще не включался в файл, то имя TVECTOR-HEADER_H определяется директивой #define.
+*/
+
+template <class my_type>
+struct node {
+	my_type info; // информационное поле узла дерева
+	int count;
+	node* left, * right;
+};
+
+template <class my_type> class Tree {
+public:
+	node* root;
+	Tree();
+	//Tree(int t); // формирование дерева из t случайных чисел
+	void copy_tree(node*& root_new, node* root_old);
+	/* Копирует дерево с корнем root_old в дерево с корнем root_new. В результате деревья находятся
+	в различных участках памяти */
+	Tree(const Tree& ob); // конструктор копирования
+	void del_tree(node* vertex);
+	/* рекурсивная функция, используемая в деструкторе(освобождение памяти) */
+	~Tree();
+	void push(node*& vertex, my_type data); // вставка элемента в дерево
+	void show(node* vertex); // вывод дерева на экран
+	node* find(node* vertex, my_type key); // поиск по ключу
+	void print_leaves(node* vertex); // вывод листьев дерева на экран
+	int get_height(node* vertex);
+	node* get_max(node* vertex);
+};
+
+template <class my_type>
+void print_node(node<my_type>* vertex);
+void show_menu();
+#endif
