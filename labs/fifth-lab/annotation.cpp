64623add1d722c0f05b6a9e460dcda9e6e5109be
Andrey Shpak <anshpak.by@gmail.com>
2022-11-29 03:15:44 +0300

    feat: add Annotation class

---
+++ labs/fifth-lab/annotation.cpp
@@ -0,0 +1,40 @@
+#include "annotation.h"
+#include <iostream>
+#include <string>
+
+void Annotation::set_name(string name) {
+	this->name = name;
+}
+
+void Annotation::set_theme(string theme) {
+	this->theme = theme;
+}
+
+void Annotation::set_author(string author) {
+	this->author = author;
+}
+
+void Annotation::set_authors_age(int age) {
+	this->authors_age = age;
+}
+
+string Annotation::get_name() {
+	return this->name;
+}
+
+string Annotation::get_theme() {
+	return this->theme;
+}
+
+string Annotation::get_author() {
+	return this->author;
+}
+
+int Annotation::get_authors_age() {
+	return this->authors_age;
+}
+
+void Annotation::vanish_text() {
+	this->set_text("");
+	// Also could be done through protected
+}
--- labs/fifth-lab/annotation.h
+++ labs/fifth-lab/annotation.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef ANNOTATION_H
+#define ANNOTATION_H
+#include "text.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Annotation : public Text {
+	string name;
+	string theme;
+	string author;
+	int authors_age;
+public:
+	void set_name(string name);
+	void set_theme(string theme);
+	void set_author(string author);
+	void set_authors_age(int age);
+	string get_name();
+	string get_theme();
+	string get_author();
+	int get_authors_age();
+	void vanish_text();
+};
+
+#endif
